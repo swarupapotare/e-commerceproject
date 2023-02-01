37ac29d07fe79ba43705ab62dc77ceda97c22df0
swarupa potare <swarupapotare@gmail.com>
2023-02-01 23:10:00 +0530

    header with css,routing home,seller done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SellerAuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
